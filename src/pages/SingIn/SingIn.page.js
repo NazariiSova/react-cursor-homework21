766cd5d8f411154c064e9b56522d4eb331816931
Nazarii Sova <madrigalamar@gmail.com>
2023-02-25 12:40:28 +0200

    add logic in hw21

--- src/pages/SingIn/SingIn.page.js
+++ src/pages/SingIn/SingIn.page.js
@@ -2,34 +2,72 @@ import React, { useState } from "react";
 import styles from "../style/SingAll.module.css"
 
 import iconLock from "../../img/icon-lock.png"
-import { Link, useNavigate } from "react-router-dom";
+import { Link, } from "react-router-dom";
 
 
 const SingInPage = () => {
-    const navigateTo = useNavigate()
+
+
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const [emailDirty, setEmailDirty] = useState('')
-    const [passwordDirty, setPasswordDirty] = useState('')
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(' Password *')
 
-    const [emailError, setEmailError] = useState('')
-    const [passwordError, setPasswordError] = useState('')
+    const [checkbox, setCheckbox] = useState(false)
 
     const handlerSingIn = () => {
-        window.alert("Sing in")
+        if (emailError === false && passwordError === false){
+           window.alert("Sing in") 
+        }
+        
     };
+    
+
 
     const handlerChange = (event) => {
-        setEmail(event.target.value)
-        const inputChange = event.target.value;
-        if (inputChange.length >= 3){
-            
-        }
+        const name = event.target.name
+        console.log(name);
+        if (name === "email") {
+             setEmail(event.target.value)
+             if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(event.target.value) ) {
+               
+                setEmailError(false)
+             } else {
+                setEmailError(true)
+             }
+
+        }else if(name === "password"){
+            setPassword(event.target.value)
+            if (!/^[а-яА-Яa-zA-Z0-9!@#$%^&*]{6,16}$/i.test(event.target.value)) {
+                setPasswordError(false)
+               
+             } else {
+                setPasswordError(true)
+             }
+        };
+
+        console.log(event);
+       
+        
+        const inputForm = event.target.value;
+        console.log(inputForm);
+
     }
 
+
+
+
+
+    const handlerCheckbox = (event) => {
+setCheckbox(event.target.checked)
+console.log(event);
+    }
+
+
     return (
-        <div className={styles["container"]}>
+        <form className={styles["container"]}>
             <div className={styles["header-sing-in"]}>
 
                 <img className={styles["header-sing-in-icon"]} src={iconLock} alt="icon-lock" />
@@ -41,13 +79,15 @@ const SingInPage = () => {
 
             <div className={styles["container-inputs"]}>
 
-                <input type="email" onChange={handlerChange} className={styles["email-input"]} placeholder=" Email Address *" value={email} />
-                <input className={styles["password-input"]} placeholder=" Password *"></input>
-                <div className={styles["container-checkbox-input"]} type="checkbox">
+                <input type="email" name="email" onChange={handlerChange} className={emailError === true ? styles["email-input"] : styles["email-error-input"]} placeholder=" Email Address *" value={email} />
+
+                <input type="password" name="password" onChange={handlerChange} className={passwordError === true ? styles["password-input"] : styles["password-error-input"]} placeholder=" Password *" value={password} />
+
+                <div onChange={handlerCheckbox} className={styles["container-checkbox-input"]} type="checkbox" checked={checkbox}>
 
                     <label htmlFor="id1">
                         <input className={styles["checkbox-input"]} id="id1" type="checkbox"></input>
-                        <span className={styles["fake"]}></span>
+                        <span className={checkbox === true ? styles["checked"] : styles["fake"]}></span>
                         <span className={styles["checkbox-text"]}>Remember me</span>
                     </label>
                 </div>
@@ -55,7 +95,7 @@ const SingInPage = () => {
 
             </div>
 
-            <button className={styles["button-sing-up"]} onClick={handlerSingIn}>Sing in</button>
+            <button type="submit" className={styles["button-sing-up"]} onClick={handlerSingIn}>Sing in</button>
 
 
             <div className={styles["container-text-under-button"]}>
@@ -66,7 +106,7 @@ const SingInPage = () => {
                 <h5>Copyright © Your Webside 2020</h5>
             </div>
 
-        </div>
+        </form>
     );
 };
 
--- src/pages/SingUp/SingUp.page.js
+++ src/pages/SingUp/SingUp.page.js
@@ -3,9 +3,102 @@ import styles from "../style/SingAll.module.css"
 import style from "./SingUp.module.css"
 import iconLock from "../../img/icon-lock.png"
 import { Link, } from "react-router-dom";
+import { useState } from "react";
+
 
 
 const SingUpPage = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+
+    const [firstNameError, setFirstNameError] = useState(false)
+    const [lastNameError, setLastNameError] = useState(false)
+
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(' Password *')
+
+    const [checkbox, setCheckbox] = useState(false)
+
+    const handlerSingUp = () => {
+        if (emailError === false && passwordError === false){
+           window.alert("Sing Up") 
+        }
+        
+    };
+    
+
+
+    const handlerChange = (event) => {
+        const name = event.target.name
+        console.log(name);
+        if (name === "email") {
+             setEmail(event.target.value)
+             if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(event.target.value) ) {
+               
+                setEmailError(false)
+             } else {
+                setEmailError(true)
+             }
+
+        }else if(name === "password"){
+            setPassword(event.target.value)
+            if (!/^[а-яА-Яa-zA-Z0-9!@#$%^&*]{6,16}$/i.test(event.target.value)) {
+                setPasswordError(false)
+               
+             } else {
+                setPasswordError(true)
+             }
+        };
+
+        console.log(event);
+       
+        
+        const inputForm = event.target.value;
+        console.log(inputForm);
+
+    }
+
+    const nameChange = (event) =>{
+        const name = event.target.name
+        console.log(name);
+        if (name === "firstName") {
+            setFirstName(event.target.value)
+             if (!/^[а-яА-Яa-zA-Z0-9]{2,20}$/i.test(event.target.value)) {
+               
+                setFirstNameError(false)
+             } else {
+                setFirstNameError(true)
+             }
+
+        }else if(name === "lastName"){
+            setLastName(event.target.value)
+            if (!/^[а-яА-Яa-zA-Z0-9]{2,20}$/i.test(event.target.value)) {
+                setLastNameError(false)
+               
+             } else {
+                setLastNameError(true)
+             }
+        };
+
+        console.log(event);
+       
+        
+        const inputForm = event.target.value;
+        console.log(inputForm);
+    }
+
+
+
+
+
+    const handlerCheckbox = (event) => {
+setCheckbox(event.target.checked)
+console.log(event);
+    }
     return (
         <div className={style["container"]}>
             <div className={styles["header-sing-in"]}>
@@ -19,17 +112,18 @@ const SingUpPage = () => {
 
             <div className={style["container-inputs"]}>
                 <div className={style["container-name-inputs"]}>
-                <input  className={style["firstname-input"]} placeholder=" First name *" />
-                <input className={style["lastname-input"]} placeholder=" Last name *"></input>  
+                <input name="firstName" className={firstNameError === true ? style["firstname-input"] : style["firstname-error-input"]} onChange={nameChange} value={firstName} placeholder=" First name *" />
+                <input name="lastName" className={lastNameError === true ? style["lastname-input"] : style["lastname-error-input"]} onChange={nameChange} value={lastName} placeholder=" Last name *"></input>  
                 </div>
 
-                <input type="email" className={styles["email-input"]} placeholder=" Email Address *" />
-                <input className={styles["password-input"]} placeholder=" Password *"></input>
+                <input type="email" name="email" onChange={handlerChange} className={emailError === true ? styles["email-input"] : styles["email-error-input"]} placeholder=" Email Address *" value={email} />
+
+<input type="password" name="password" onChange={handlerChange} className={passwordError === true ? styles["password-input"] : styles["password-error-input"]} placeholder=" Password *" value={password} />
                 <div className={style["container-checkbox-input"]} type="checkbox">
 
                     <label htmlFor="id1">
-                        <input className={styles["checkbox-input"]} id="id1" type="checkbox"></input>
-                        <span className={styles["fake"]}></span>
+                        <input onChange={handlerCheckbox} className={styles["checkbox-input"]} id="id1" type="checkbox" checked={checkbox}></input>
+                        <span className={checkbox === true ? styles["checked"] : styles["fake"]}></span>
                         <div className={style["container-span"]}>
                             <span className={style["checkbox-text"]}>I want to receive inspiration, marceting </span>
                             <span className={style["checkbox-text"]}>promotions and updates via email</span>
@@ -41,7 +135,7 @@ const SingUpPage = () => {
 
             </div>
 
-            <button className={style["button-sing-up"]}>Sing up</button>
+            <button onClick={handlerSingUp} className={style["button-sing-up"]}>Sing up</button>
 
 
             <div className={style["container-text-under-button"]}>
